0cbd5c9c65e98d7f25b66c5fdc0ab6c367699ba3
Allison Valdez <ajsnipes@usfca.edu>
2022-12-19 11:45:52 -0500

    stopping errors

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -1,4 +1,10 @@
-describe("Servers test (with setup and tear-down)", function() {
+/*
+Questions:
+1. how to check if not a number different methods not working
+2.
+*/
+
+describe("Servers test (with setup and tear-down)", function () {
   beforeEach(function () {
     // initialization logic
     serverNameInput.value = 'Alice';
@@ -12,6 +18,24 @@ describe("Servers test (with setup and tear-down)", function() {
   });
 
   afterEach(function() {
-    // teardown logic
+    serverNameInput.value = "";
+    allServers = {};
+  });
+});
+
+describe("Testing the submitServerInfo function", function () {
+  it("should not allow a number entry", function () {
+    //expect(serverNameInput).toBeNaN();
+    //expect(submitServerInfo).not.toEqual('number');
+    //expect(serverName).not.toEqual('number');
+    //expect(serverName).not.toEqual(jasmine.any(Number));
+    expect(typeof serverName).not.toEqual(Number);
+  });
+  
+});
+
+describe("Testing the updateServerTable function", function () {
+  it("should only allow for number entry", function () {
+    
   });
 });
